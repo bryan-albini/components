9bd83a6ce4e0112e3c11d586843996394dd724cb
Bryan Albini <balbini@Bryans-MacBook-Pro.local>
2020-06-10 08:55:07 -0700

    practicing passing props, fixed dependency issue with websocket extension

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">
-                        Nice post!
-                    </div>
-                </div>
-            </div>
+            <CommentDetail author="Sam" timeAgo="Today at 4:30PM" post="Cool" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Alex" timeAgo="Today at 2:30PM" post="Sweet" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Mack" timeAgo="Today at 5:30PM" post="Radical" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Jane" timeAgo="Today at 7:30PM" post="Gnarly" avatar={faker.image.avatar()}/>
         </div>
     );
 };
