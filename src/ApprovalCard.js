1fab21df781e3ed106737f41211678c1563088bb
Bryan Albini <balbini@Bryans-MacBook-Pro.local>
2020-06-10 17:52:31 -0700

    adding approval card component

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ApprovalCard = (props) => {
-    
+    // testing
     return (
         <div className="ui card">
             <div className="content">{props.children}</div>
